17be9ae6fff8636bb35d17c8c171b8dffdc0ca5c
Eva Barrett <Evabarrett23@Gmail.com>
2018-11-14 22:24:24 -0800

    grabbed input values

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
 let button = document.getElementById('button');
 let addFave = function(e) {
-  console.log('event: ', e);
+  let siteName = document.getElementById('site-name');
+  let siteUrl = document.getElementById('site-url');
+  let name = siteName.value;
+  let url = siteUrl.value;
+  console.log('url: ', url);
+  console.log('Name: ', name);
 }
 
 button.addEventListener('click', function(event) {
