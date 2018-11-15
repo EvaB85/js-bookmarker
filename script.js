8d722d68595d026f9a5fbfb9b6c466935718b835
Eva Barrett <Evabarrett23@Gmail.com>
2018-11-15 14:02:09 -0800

    visit link works with http hardcoded

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 
 let button = document.getElementById('button');
 
+let urlContent = function() {
+  let siteUrl = document.getElementById('site-url');
+  let url = 'http://' + siteUrl.value;
+  return url;
+}
+
 let addFave = function(e) {
   let faveBox = document.getElementById('fave-box');
   let siteName = document.getElementById('site-name');
-
-  let siteUrl = document.getElementById('site-url');
-  let url = siteUrl.value;
-  
   let name = siteName.value;
   let outerDiv = document.createElement('div');
   outerDiv.className = 'fave';
   let newH4 = document.createElement('h4');
   newH4.textContent = name;
   outerDiv.append(newH4);
-
   let innerDiv = document.createElement('div');
   innerDiv.className = 'btn-wrap';
-
   let visitButton = document.createElement('button');
   visitButton.textContent = 'Visit';
-  visitButton.addEventListener('click', function() {
-    // window.open(url);
-
-  });
-
   let deleteButton = document.createElement('button');
   deleteButton.textContent = 'Delete';
+
   deleteButton.addEventListener('click', function() {
     outerDiv.remove();
+  });
+
+  visitButton.addEventListener('click', function() {
+    window.open( urlContent() );
   });
 
   innerDiv.append(visitButton);
